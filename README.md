b3ffba3dd03e562b29b64b8a3bbe76ea4f570de8
xiaoxunyao <huchengyea@163.com>
2022-10-01 22:32:19 +0800

    chore: init first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<div align="center">
+<img src="./public/logo.svg" wigth='100px' height='100px'>
+</div>
+
+<div align="center">
+<h1>LogonTracer</h1>
+</div>
+
+## Core functions:
+
+- Monitor the specified exception thrown in the log or the specified exception thrown in the method and class, send it to the mailbox, etc.
+
+- Support mainstream frameworks such as log4j, log4j2, and logback.
+
+- Support SpringBoot, SpringMVC and other frameworks.
---
+++ docs/.vitepress/meta.ts
@@ -0,0 +1,14 @@
+export const name = 'logon tracer'
+export const shortName = 'logon tracer'
+export const description = 'Monitor the specified exception thrown in the log or the specified exception thrown in the method and class'
+
+/* CDN fonts and styles */
+export const googleapis = 'https://fonts.googleapis.com'
+export const gstatic = 'https://fonts.gstatic.com'
+export const font = `${googleapis}/css2?family=Readex+Pro:wght@200;400;600&display=swap`
+
+/* GitHub and social links */
+export const github = 'https://github.com/elonehoo/logon-tracer'
+export const releases = 'https://github.com/elonehoo/logon-tracer/releases'
+export const discord = 'https://discord.gg/Fry7332ar7'
+export const twitter = 'https://twitter.com/huchengye'
