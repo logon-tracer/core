f81b1c31329ab4f232b3ef9af399084ce6c3ea4f
xiaoxunyao <huchengyea@163.com>
2022-10-14 19:50:48 +0800

    chore: Add Example for Logback

---
+++ example/spring-boot-logback/src/main/java/org/example/exception/TestAspectException.java
@@ -0,0 +1,4 @@
+package org.example.exception;
+
+public class TestAspectException extends RuntimeException {
+}
---
+++ example/spring-boot-logback/src/main/java/org/example/exception/TestExtendsRuntimeException.java
@@ -0,0 +1,6 @@
+package org.example.exception;
+
+import logon.tracer.exception.AlarmLogRuntimeException;
+
+public class TestExtendsRuntimeException extends AlarmLogRuntimeException {
+}
